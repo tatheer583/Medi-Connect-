7ed38ee2c92d386a358c1c65ebe5d5a6e016c4c4
tatheer583 <tatheer583@users.noreply.github.com>
2026-05-28 18:47:12 +0500

    update app config and add mediconnect-v1.0.0.zip release

--- mobile/lib/src/app.dart
+++ mobile/lib/src/app.dart
@@ -17,9 +17,9 @@ class MediConnectApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       builder: (context, child) {
         return MediaQuery(
-          data: MediaQuery.of(context).copyWith(
-            textScaleFactor: 1.0,
-          ),
+          data: MediaQuery.of(
+            context,
+          ).copyWith(textScaler: TextScaler.linear(1.0)),
           child: child ?? const SizedBox.shrink(),
         );
       },
